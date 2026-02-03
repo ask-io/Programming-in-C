97471fc484fd63b466a6667ed08e861e188b9b3a
Abhijit S Kunnel <abhijit.uni29@gmail.com>
2026-02-03 18:48:01 +0400

    Refactor matrix addition program for clarity

--- Programming in C/01_28_2026/addmatrix.c
+++ Programming in C/01_28_2026/addmatrix.c
@@ -1,18 +1,42 @@
 //To add elements of two matrices
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-        int arr[5][5], r, c, i, j;
+int main() {
+    int r1, c1, r2, c2, i, j;
+    int a[10][10], b[10][10], sum[10][10];
 
-        printf("Enter limit: ");
-        scanf("%d", &n);
+    printf("Enter rows and columns for first matrix: ");
+    scanf("%d %d", &r1, &c1);
+    printf("Enter rows and columns for second matrix: ");
+    scanf("%d %d", &r2, &c2);
 
-        for(i=0; i<n; i++)
-        {
-		for(j=0, j<n; j++)
-		{
-        	        printf("Enter value %d: ", j+1);
-	                scanf("%d %d", &arr[i][j]);
+    if (r1 == r2 && c1 == c2) {
+        printf("Enter elements of first matrix:\n");
+        for (i = 0; i < r1; i++) {
+            for (j = 0; j < c1; j++) {
+                scanf("%d", &a[i][j]);
+            }
         }
+
+        printf("Enter elements of second matrix:\n");
+        for (i = 0; i < r2; i++) {
+            for (j = 0; j < c2; j++) {
+                scanf("%d", &b[i][j]);
+            }
+        }
+
+        printf("\nResultant Matrix (Sum):\n");
+        for (i = 0; i < r1; i++) {
+            for (j = 0; j < c1; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+                printf("%d\t", sum[i][j]);
+            }
+            printf("\n");
+        }
+    } else {
+        printf("Matrix addition of different orders is not allowed.\n");
+    }
+
+    return 0;
+}
 
