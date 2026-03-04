f79fb14a26e85a4227a301e3d85ab89caa8db879
Abhijit S Kunnel <abhijit.uni29@gmail.com>
2026-03-04 23:21:12 +0530

    Implement character frequency count in string
    Added character frequency counting functionality that ignores case sensitivity and non-alphabetic characters.

--- Programming in C/02_25_2026/strfreq.c
+++ Programming in C/02_25_2026/strfreq.c
@@ -1,15 +1,24 @@
-//Q3) Character frequency count in a string ignore case sensitivity and ignore other characters
+//Character frequency count in a string ignore case sensitivity and ignore other characters
 #include <stdio.h>
 #include <ctype.h>
 
 int main(){
-	char s1[50];
-	int i;
-        printf("Enter your string: ");
-        scanf("%s", s1);
+	char s1[50], lower;
+	int i, count[26]={0};
+    printf("Enter your string: ");
+    scanf("%s", s1);
 	for(i=0; s1[i]!='\0'; i++){
-		
+		lower = tolower(s1[i]);
+		if(lower >= 'a' && lower <= 'z'){
+			count[lower - 'a']++; //(lower - ASCII value of 'a') gives index of count array
+		}
 	}
+	printf("\nCharacter Frequency:\n");
+    for(i = 0; i < 26; i++) {
+        if(count[i] > 0) {
+            printf("%c : %d\n", i + 'a', count[i]);
+		}
+	}
+
 	return 0;
 }
-
